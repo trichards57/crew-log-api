17d1bdbed4db54f6b3e7b69a517d78d24c2f6232
Tony Richards <trichards57@outlook.com>
2022-03-12 08:45:38 +0000

    Begun setting up profile management page.

--- CrewLogApi/wwwroot/js/site.js
+++ CrewLogApi/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function initialize() {
+    initializeSimplebar();
+    initializeSidebarCollapse();
+}
+
+function initializeSimplebar() {
+    const simplebarElement = document.getElementsByClassName("js-simplebar")[0];
+
+    if (simplebarElement) {
+        const simplebarInstance = new SimpleBar(document.getElementsByClassName("js-simplebar")[0]);
+
+        /* Recalculate simplebar on sidebar dropdown toggle */
+        const sidebarDropdowns = document.querySelectorAll(".js-sidebar [data-bs-parent]");
+
+        sidebarDropdowns.forEach(link => {
+            link.addEventListener("shown.bs.collapse", () => {
+                simplebarInstance.recalculate();
+            });
+            link.addEventListener("hidden.bs.collapse", () => {
+                simplebarInstance.recalculate();
+            });
+        });
+    }
+}
+
+function initializeSidebarCollapse() {
+    const sidebarElement = document.getElementsByClassName("js-sidebar")[0];
+    const sidebarToggleElement = document.getElementsByClassName("js-sidebar-toggle")[0];
+
+    if (sidebarElement && sidebarToggleElement) {
+        sidebarToggleElement.addEventListener("click", () => {
+            sidebarElement.classList.toggle("collapsed");
+
+            sidebarElement.addEventListener("transitionend", () => {
+                window.dispatchEvent(new Event("resize"));
+            });
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    feather.replace();
+    initialize();
+});
